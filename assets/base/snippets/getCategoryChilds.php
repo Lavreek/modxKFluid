fbea6d6ccc07e6905953b6112ec998f6644a95ac
lavreek <lavreeksolacki@yandex.ru>
2022-12-16 12:04:53 +0300

    update Snippets

--- assets/base/snippets/getCategoryChilds.php
+++ assets/base/snippets/getCategoryChilds.php
@@ -5,7 +5,7 @@ if (isset($modx->resourceIdentifier)) {
     $resources = [2]; // Card - TV Params
     $pageId = $modx->resourceIdentifier;
 
-    $selectCategories = "SELECT `id`, `pagetitle` FROM `modx_site_content` WHERE `parent` = '$pageId' AND `published` = '1'";
+    $selectCategories = "SELECT `id`, `pagetitle` FROM `modx_site_content` WHERE `parent` = '$pageId' AND `published` = '1' AND `template` = '4'";
     $categories = $modx->query($selectCategories);
 
     if (!is_bool($categories)) {
@@ -14,7 +14,7 @@ if (isset($modx->resourceIdentifier)) {
         foreach ($categories as $category) {
             echo "<h3 class='page-title py-em-2'><a style='all: unset; cursor: pointer;' href='[[~{$category['id']}]]'>{$category['pagetitle']}</a></h3>";
 
-            $selectChilds = "SELECT `id`, `pagetitle` FROM `modx_site_content` WHERE `parent` = '{$category['id']}' AND `published` = '1' AND `template` = '4'";
+            $selectChilds = "SELECT `id`, `pagetitle` FROM `modx_site_content` WHERE `parent` = '{$category['id']}' AND `published` = '1'";
             $childs = $modx->query($selectChilds);
 
             if (!is_bool($childs)) {
