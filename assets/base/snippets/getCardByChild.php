594579a731d2f7861730ee1a4dcaa0e20c1e90bf
lavreek <lavreeksolacki@yandex.ru>
2022-12-29 17:26:22 +0300

    update

--- assets/base/snippets/getCardByChild.php
+++ assets/base/snippets/getCardByChild.php
@@ -9,7 +9,7 @@ global $modx;
 if (isset($modx->resourceIdentifier) and $modx->resourceIdentifier > 0) {
     $page_id = $modx->resourceIdentifier;
 
-    $selectChilds = "SELECT `id`, `pagetitle` FROM `modx_site_content` WHERE `parent` = '$page_id' AND `published` = '1' AND `template` in (". implode(",", $acceptedTemplates) .")";
+    $selectChilds = "SELECT `id`, `pagetitle`, `template` FROM `modx_site_content` WHERE `parent` = '$page_id' AND `published` = '1' AND `template` in (". implode(",", $acceptedTemplates) .")";
     $childs = $modx->query($selectChilds);
 
     if (!is_bool($childs)) {
@@ -43,7 +43,25 @@ if (isset($modx->resourceIdentifier) and $modx->resourceIdentifier > 0) {
                 }
             }
 
-            $childCard .= $modx->getChunk('product_card', $row);
+            /**
+             * Наложение pdf ссылки поверх карточки шаблона: "Шаблон продукции в виде файла"
+             */
+            if ($row['template'] == 6) {
+                $selectLink = "SELECT `value` FROM `modx_site_tmplvar_contentvalues` WHERE `tmplvarid` = '11'";
+                $pdfLink = $modx->query($selectLink);
+
+                if (!is_bool($pdfLink)) {
+                    $pdfLink = $pdfLink->fetch(PDO::FETCH_ASSOC);
+
+                    if (isset($pdfLink['value'])) {
+                        $row += ['url' => $pdfLink['value']];
+                        $childCard .= $modx->getChunk('product_card_file', $row);
+                    }
+                }
+                
+            } else {
+                $childCard .= $modx->getChunk('product_card', $row);
+            }
         }
 
         if ($childCard != "") {
